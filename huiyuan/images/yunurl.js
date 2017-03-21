62aa184d8a5af79a2633f373f5b2e1f5a935b94a
iq96552156 <iq96552156@163.com>
2017-03-22 01:13:23 +0800

    1.,1.1.1.yun.js

---
+++ huiyuan/images/yunurl.js
@@ -0,0 +1,3 @@
+var WxYunUrl="https://chenshujucntv2.github.io/ali0321/dzp.html";//大转盘
+var WxYunUrl1="http://note.youdao.com/noteshare?id=dfd3d432dae3220d918b2b274fee81d4";//分享内容
+var WxYunUrl2="http://note.youdao.com/noteshare?id=dfd3d432dae3220d918b2b274fee81d4";//分享内容
