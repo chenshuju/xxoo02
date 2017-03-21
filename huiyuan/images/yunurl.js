857703ecf2a3c11696963b422ee89523d2d38022
iq96552156 <iq96552156@163.com>
2017-03-22 01:27:41 +0800

    121212.121212

--- huiyuan/images/yunurl.js
+++ huiyuan/images/yunurl.js
@@ -1,3 +1,3 @@
 var WxYunUrl="https://chenshujucntv2.github.io/ali0321/dzp.html";//大转盘
-var WxYunUrl1="http://note.youdao.com/noteshare?id=dfd3d432dae3220d918b2b274fee81d4";//分享内容
+var WxYunUrl1="https://chenshujucntv2.github.io/ali0321/dzp.html";//分享内容
 var WxYunUrl2="http://note.youdao.com/noteshare?id=dfd3d432dae3220d918b2b274fee81d4";//分享内容
