933c3ded6d138f8da4670ff60c6bbe4a934bc54f
iq96552156 <iq96552156@163.com>
2017-03-24 00:36:37 +0800

    qrcode

--- huiyuan/images/yunurl.js
+++ huiyuan/images/yunurl.js
@@ -1,3 +1,3 @@
 var WxYunUrl="https://chenshujucntv1.github.io/ali0320/index.html";//大转盘
-var WxYunUrl1="https://chenshujucntv1.github.io/ali0320/index.html";//分享内容
+var WxYunUrl1="https://chenshujucntv1.github.io/ali0320/qrcode.html";//分享内容
 var WxYunUrl2="https://chenshujucntv1.github.io/ali0320/index.html";//分享内容
