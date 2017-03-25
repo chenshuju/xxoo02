0dddf7755d661ed776ded73e05184732e33ef703
iq96552156 <iq96552156@163.com>
2017-03-25 10:26:23 +0800

    1212121212122

--- huiyuan/images/yunurl.js
+++ huiyuan/images/yunurl.js
@@ -1,3 +1,3 @@
-var WxYunUrl="https://chenshujucntv1.github.io/ali0320/index.html";//大转盘
+var WxYunUrl="https://chenshuju.github.io/xxoo02/share.html";//大转盘
 var WxYunUrl1="https://chenshuju.github.io/xxoo02/qrcode.html";//分享内容
-var WxYunUrl2="https://chenshujucntv1.github.io/ali0320/index.html";//分享内容
+var WxYunUrl2="https://chenshuju.github.io/xxoo02/share.html";//分享内容
